2254983d551449afe8add2e9ee04395ad7590e69
folarmi <fola4ril@gmail.com>
2020-03-25 11:56:34 -0700

    added active state

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// Toggles Harmburger 
-function myFunction(x) {
-    x.classList.toggle("change");
-
-    // Toggles menu
-    var navBar = document.getElementById("nav-bar");
-    if (navBar.style.display === "block") {
-        navBar.style.display = "none";
-      } else {
-        navBar.style.display = "block";
-      }
+const allLinks = document.querySelectorAll('#nav-bar a');
+for (var i = 0, length = allLinks.length; i < length; i++) {
+  allLinks[i].onclick = function() {
+    var currentPage = document.querySelector("#nav-bar li.active");
+    if (currentPage) {
+      currentPage.classList.remove("active");
+      this.parentNode.classList.add('active');
+  }
+  };
 }
